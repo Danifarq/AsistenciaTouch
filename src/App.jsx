1bfd50cd65794911bfdb9905e4181635aa9b869a
MateoMzc <mateocontreras789@gmail.com>
2025-06-12 21:37:46 -0300

    Arreglos en pantalla de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-<<<<<<< HEAD
 import './index.css'; 
 
 function App() {
   return (
     <div className="container">
       <h1>BIENVENIDO</h1>
-      <label htmlFor="dni">INGRESE DNI:</label>
-      <input type="text" id="dni" placeholder="123456789" />
-=======
-import Inicio from './Inicio';
-import './App.css';
-import Registro from './Registro';
 
-function App() {
-  return (
-    <div className="Inicio">
-      <header className="App-header">
-        <p>
-         
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 169c141a794ec597b93e1c95fbc69fa24597e4d4
+      <div className="form-row">
+        <label htmlFor="dni">INGRESE DNI:</label>
+        <input type="text" id="dni"/>
+      </div>
+
+      <div className="form-row">
+        <label htmlFor="password">CONTRASEÑA:</label>
+        <input type="password" id="password" />
+      </div>
+
+      <button>ENVIAR</button>
     </div>
   );
 }
