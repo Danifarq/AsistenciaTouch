124b3f5c306810a660c76ff55ac97333dff6037e
MateoMzc <mateocontreras789@gmail.com>
2025-08-30 13:13:27 -0300

    Crando pantalla principal

---
+++ src/components/MenuPrincipal.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "../index.css";
+
+const MenuPrincipal = () => {
+  const [cursoSeleccionado, setCursoSeleccionado] = useState("");
+  const cursos = ["6° 2°", "1° 3°", "1° 2°"];
+
+  return (
+    <div className="menu-principal-container">
+      <label className="curso-label">CURSO:</label>
+      <select
+        className="curso-select"
+        value={cursoSeleccionado}
+        onChange={(e) => setCursoSeleccionado(e.target.value)}
+      >
+        <option value="">Seleccione un curso</option>
+        {cursos.map((curso, index) => (
+          <option key={index} value={curso}>
+            {curso}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default MenuPrincipal;
