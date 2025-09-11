eb3e48e190d03382090da0e5afacde62a9c91b83
Danifarq <danifarq13@gmail.com>
2025-09-11 19:27:53 -0300

    Agregar la comprobacion de datos

--- src/hooks/useMaterias.jsx
+++ src/hooks/useMaterias.jsx
@@ -4,3 +4,10 @@ const col= collection(db, "materias");
 const snap = await getDocs(col);
 const dataMaterias = snap.docs.map((doc) => ({ id: doc.id, ...doc.dataMaterias() }));
 console.log(dataMaterias);
+//-----Suscribirse a un doc -----
+function listenById (id, cd, errCb){
+    const ref = doc(BroadcastChannel, "materias", id);
+    return onSnapshot (ref,(d)=> {
+    cd (d.exist() ? {id: d.id, ...d.data()}:null);
+    }, errCb);
+}
