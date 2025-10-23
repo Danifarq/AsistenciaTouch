73a0adb1092dd6aa7574419b18b522fe3523ad37
Danifarq <danifarq13@gmail.com>
2025-10-23 10:44:09 -0300

    Organizando las protecciones

--- src/components/ProteccionRutas.jsx
+++ src/components/ProteccionRutas.jsx
@@ -2,9 +2,26 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
-const ProteccionRutas = ({ children }) => {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/" />;
-};
+import { Navigate } from "react-router-dom";
 
+const PrivateRoute = ({ children, roleRequired }) => {
+  const { user, userRole, loading } = useAuth();
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (!user) {
+    // Si no está logueado, redirige al login
+    return <Navigate to="/login" />;
+  }
+
+  // Verifica el rol si es necesario
+  if (roleRequired && userRole !== roleRequired) {
+    // Si el rol no coincide, redirige a una página de inicio o muestra un mensaje de error
+    return <Navigate to="/inicio" />;
+  }
+
+  return children;
+};
 export default ProteccionRutas;
--- src/pages/routes/Approute.jsx
+++ src/pages/routes/Approute.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import  { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth'; 
+
 import Login from '../../components/Login.jsx';
 import Inicio from "../../components/Inicio";
 import Registro from "../../components/Registro";
 import MenuPrincipal from '../../components/MenuPrincipal';
 import AltaProfesor from '../../components/AltaProfesor';
-// Componente para proteger rutas privadas
-const PrivateRoute = ({ children, roleRequired }) => {
-  const { user, userRole, loading } = useAuth();
-
-  if (loading) {
-    return <div>Cargando...</div>;
-  }
-
-  if (!user) {
-    // Si no está logueado, redirige al login
-    return <Navigate to="/login" />;
-  }
-
-  // Verifica el rol si es necesario
-  if (roleRequired && userRole !== roleRequired) {
-    // Si el rol no coincide, redirige a una página de inicio o muestra un mensaje de error
-    return <Navigate to="/inicio" />;
-  }
-
-  return children;
-};
+import ProteccionRutas from "../../components/ProteccionRutas.jsx";
 
 const Approute = () => {
  return (
  <Routes>
-  
+     {/* Rutas públicas */}
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/registro" element={<Registro />} />
 
       //* Rutas protegidas que requieren un usuario logueado
-      <Route path="/inicio" element={<PrivateRoute><Inicio /></PrivateRoute>} />
-      <Route path="/menuprincipal" element={<PrivateRoute><MenuPrincipal /></PrivateRoute>} />
+      <Route path="/inicio" element={<ProteccionRutas><Inicio /></ProteccionRutas>} />
+      <Route path="/menuprincipal" element={<ProteccionRutas><MenuPrincipal /></ProteccionRutas>} />
       
       //*Ejemplo de rutas protegidas por roles
-      <Route path="/alta-profesor" element={<PrivateRoute roleRequired="profesor"><AltaProfesor /></PrivateRoute>} />
+      <Route path="/alta-profesor" element={<ProteccionRutas roleRequired="profesor"><AltaProfesor /></ProteccionRutas>} />
       //* En caso de que se intente acceder a una ruta inexistente, redirigir al inicio
       <Route path="*" element={<Navigate to="/inicio" />} />
     </Routes>
