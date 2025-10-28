275e39db4ab603d75db2663ec8644634f5d37dd6
Danifarq <danifarq13@gmail.com>
2025-10-28 02:06:43 -0300

    Agregando la opción de agregar materia y curso en registro

--- src/hooks/useProfesoresList.jsx
+++ src/hooks/useProfesoresList.jsx
@@ -1,4 +1,4 @@
-// src/hooks/useProfesoresList.js
+
 import { useState, useEffect } from "react";
 import { db } from "../firebase/firebase";
 import { collection, getDocs } from "firebase/firestore";
@@ -10,11 +10,20 @@ export const useProfesoresList = () => {
   useEffect(() => {
     const obtenerProfesores = async () => {
       try {
+        
         const querySnapshot = await getDocs(collection(db, "profesores"));
-        const lista = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
+
+    
+        const lista = querySnapshot.docs
+          .map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+            materias: Array.isArray(doc.data().materias)
+              ? doc.data().materias
+              : [],
+          }))
+          .filter((u) => u.rol === "profesor" || u.rol === "usuario");
+
         setProfesores(lista);
       } catch (error) {
         console.error("Error al obtener profesores:", error);
