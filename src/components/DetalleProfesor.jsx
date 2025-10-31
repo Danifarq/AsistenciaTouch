5c58dd610420b561eb76e7021ce8fc1aa8279bed
SantiGolpe <santiagogolpe43@gmail.com>
2025-10-31 12:19:08 -0300

    Agregando el estilo terminado a Detalle Profesor

--- src/components/DetalleProfesor.jsx
+++ src/components/DetalleProfesor.jsx
@@ -1,14 +1,13 @@
-
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase/firebase";
 import { doc, getDoc } from "firebase/firestore";
-import BotonRedirigir from '../components/BotonRedirigir';
-
+import BotonRedirigir from "../components/BotonRedirigir";
+import '../css/DetalleProfesor.css'; 
 const DetalleProfesor = () => {
   const { id } = useParams();
   const [profesor, setProfesor] = useState(null);
-   const [cargando, setCargando] = useState(true);
+  const [cargando, setCargando] = useState(true);
 
   useEffect(() => {
     const obtenerProfesor = async () => {
@@ -17,25 +16,30 @@ const DetalleProfesor = () => {
       if (docSnap.exists()) {
         setProfesor(docSnap.data());
       }
-       setCargando(false);
+      setCargando(false);
     };
     obtenerProfesor();
   }, [id]);
 
- if (cargando) return <p>Cargando datos...</p>;
- if (!profesor) return <p>Profesor no encontrado.</p>;
+  if (cargando) return <p>Cargando datos...</p>;
+  if (!profesor) return <p>Profesor no encontrado.</p>;
+
   return (
-    <div>
-      <h2>Detalle del Profesor</h2>
-      <p><strong>Nombre:</strong> {profesor.nombre}</p>
-      <p><strong>Materia:</strong> {profesor.materia}</p>
-      <p><strong>Email:</strong> {profesor.email}</p>
-       <BotonRedirigir 
-                    textoBoton="IR A PANEL ADMIN" 
-                    ruta="/menuprincipal" 
-                />
+    <div className="detalle-profesor-page">
+      <div className="detalle-profesor-box">
+        <h2 className="detalle-profesor-title">Detalle Profesor</h2>
+
+        <div className="detalle-profesor-info">
+          <p><strong>Nombre:</strong> {profesor.nombre}</p>
+          <p><strong>Materia:</strong> {profesor.materia}</p>
+          <p><strong>Correo:</strong> {profesor.email}</p>
+        </div>
+
+        <div className="volver-panel">
+          <BotonRedirigir textoBoton="Ir a Panel Admin" ruta="/menuprincipal" />
+        </div>
+      </div>
     </div>
-   
   );
 };
 
--- src/pages/routes/Approute.jsx
+++ src/pages/routes/Approute.jsx
@@ -14,6 +14,7 @@ import AltaMateria from '../../components/AltaMateria.jsx';
 import BajaMateria from '../../components/BajaMateria.jsx';
 import AltaCurso from '../../components/AltaCurso.jsx';
 import BajaCurso from '../../components/BajaCurso.jsx';
+
 const Approute = () => {
  return (
  <Routes>
@@ -36,6 +37,7 @@ const Approute = () => {
       <Route path="/baja-materia" element={<ProteccionRutas roleRequired="admin"><BajaMateria /></ProteccionRutas>} />
       <Route path="/alta-curso" element={<ProteccionRutas roleRequired="admin"><AltaCurso /></ProteccionRutas>} />
       <Route path="/baja-curso" element={<ProteccionRutas roleRequired="admin"><BajaCurso /></ProteccionRutas>} />
+      
       //* En caso de que se intente acceder a una ruta inexistente, redirigir al inicio
       <Route path="*" element={<Navigate to="/inicio" />} />
     </Routes>
