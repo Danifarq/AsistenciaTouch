f1db85155cb19bbe47a330195563e4b8859fdbbd
santisantoianni <santiagourrea2018@gmail.com>
2025-10-31 05:30:46 -0300

    agregando estilo y modificando js de pagina de confirmacion de asistencia

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,42 +1,19 @@
-
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { signInWithPopup } from 'firebase/auth';
-import { auth, googleProvider } from '../firebase/firebase.jsx';
 import BotonRedirigir from '../components/BotonRedirigir';
-import '../index.css';
+import '../css/Inicio.css';
 
 const Inicio = () => {
   const navigate = useNavigate();
 
-  // Función para el inicio de sesión con Google
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, googleProvider);
-      const user = result.user;
-      console.log('Usuario autenticado con Google:', user);
-      localStorage.setItem('usuario', JSON.stringify({
-        correo: user.email,
-        nombre: user.displayName,
-        uid: user.uid,
-      }));
-      navigate('/MenuPrincipal');
-    } catch (error) {
-      console.error('Error al iniciar sesión con Google:', error);
-      alert('No se pudo iniciar sesión con Google.');
-    }
-  };
-
   return (
-    <div className="menu-wrapper">
-      <div>
-        <h1>ESTO ES INICIO/asistencia</h1>
-        
-        <p>ASISTENCIA CONFIRMADA</p>
+    <div className="inicio-fondo">
+      <div className="overlay"></div>
 
-        <BotonRedirigir 
-          textoBoton="IR A MENU PRINCIPAL" 
-          ruta="/menuprincipal" 
-        />
+      <div className="inicio-contenedor">
+        <h1 className="titulo">ASISTENCIA CONFIRMADA</h1>
+        <p className="subtexto">Tu asistencia ha sido registrada correctamente.</p>
+        <BotonRedirigir textoBoton="IR A MENU PRINCIPAL" ruta="/menuprincipal" />
       </div>
     </div>
   );
